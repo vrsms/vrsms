a89a675d8ea8f748922b4fd671c6bf1248136672
Kaiza Ilomo <kaiza@kunonu.co.tz>
2022-06-02 22:06:36 +0300

    Add list and filter display

--- repairs/admin.py
+++ repairs/admin.py
@@ -2,5 +2,30 @@ from django.contrib import admin
 from .models import Vehicle
 from .models import RepairTicket
 
+
 # Register your models here.
-admin.site.register(RepairTicket)
+@admin.register(RepairTicket)
+class RepairTicketAdmin(admin.ModelAdmin):
+    """Admin View for Repair Ticket"""
+
+    list_display = (
+        'title',
+        'cost',
+        'date',
+        'approval_status',
+    )
+    list_filter = (
+        'title',
+        'cost',
+        'approval_status',
+    )
+    fields = ("ref_no", "title", "driver", "vehicle", "approval_status", "item_maintained", "frequency", "cost", "date")
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+
+        if not is_superuser:
+            form.base_fields['approval_status'].disabled = True
+
+        return form
