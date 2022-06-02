d631ede420155af684dc1cd3f925503df2b26541
Kaiza Ilomo <kaiza@kunonu.co.tz>
2022-06-02 18:45:15 +0300

    Added service cost aggregation logic

--- vehicles/admin.py
+++ vehicles/admin.py
@@ -2,21 +2,28 @@
 # ==============
 
 from django.contrib import admin
+
+from services.models import ServiceTicket
 from .models import Vehicle
 
 
 @admin.register(Vehicle)
-class PostAdmin(admin.ModelAdmin):
+class VehicleAdmin(admin.ModelAdmin):
     """Admin View for Vehicle"""
 
     list_display = (
         'plate_number',
-        'model',
         'category',
         'status',
+        'total_service_cost',
     )
     list_filter = (
         'category',
         'make',
         'status',
     )
+
+    def total_service_cost(self, obj):
+        from django.db.models import Sum
+        result = ServiceTicket.objects.filter(vehicle=obj).aggregate(Sum("cost"))
+        return result["cost__sum"]
