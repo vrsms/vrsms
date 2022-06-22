aeb22d9016a0611f0fca307c4274b9e1ded5ff38
Kaiza Ilomo <kaiza@kunonu.co.tz>
2022-06-22 07:43:35 +0300

    add clickable button

--- vehicles/admin.py
+++ vehicles/admin.py
@@ -3,12 +3,16 @@
 
 from django.contrib import admin
 from django.db.models import Sum
-from django.urls import reverse
+from django.urls import reverse, path
 from django.utils.html import format_html
 from django.utils.http import urlencode
 from repairs.models import RepairTicket
 from services.models import ServiceTicket
 from .models import Vehicle
+
+import io
+from django.http import FileResponse, HttpResponseRedirect
+from reportlab.pdfgen import canvas
 
 
 @admin.register(Vehicle)
@@ -22,6 +26,7 @@ class VehicleAdmin(admin.ModelAdmin):
         'view_service_tickets_link',
         'total_repair_cost',
         'total_service_cost',
+        'download',
 
     )
     list_filter = (
@@ -29,6 +34,7 @@ class VehicleAdmin(admin.ModelAdmin):
         'make',
         'status',
     )
+    change_list_template = "vehicles/vehicles_changelist.html"
 
     def total_repair_cost(self, obj: Vehicle):
         """ Calculate the total service cost for each Vehicle object"""
@@ -73,3 +79,36 @@ class VehicleAdmin(admin.ModelAdmin):
         return format_html('<a href="{}">{} Service Tickets</a>', url, count)
 
     view_service_tickets_link.short_description = "Service Tickets"
+
+    def download(self, obj: Vehicle):
+        """ Download all service tickets linked to this vehicle"""
+
+        count = obj.serviceticket_set.count()
+
+        url = (
+                reverse("admin:services_serviceticket_changelist")
+                + "?"
+                + urlencode({"serviceticket__id": f"{obj.id}"})
+        )
+        k=67
+        return format_html('<a href="{}">{} mDownload {} Service Tickets</a>', url, count,k)
+
+    download.short_description = "Download Service Tickets"
+
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path('immortal/', self.set_immortal),
+            path('mortal/', self.set_mortal),
+        ]
+        return my_urls + urls
+
+    def set_immortal(self, request):
+        self.model.objects.all().update(is_immortal=True)
+        self.message_user(request, "All heroes are now immortal")
+        return HttpResponseRedirect("../")
+
+    def set_mortal(self, request):
+        self.model.objects.all().update(is_immortal=False)
+        self.message_user(request, "All heroes are now mortal")
+        return HttpResponseRedirect("../")
