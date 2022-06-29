e38f0ec47ad7ff18b82c9dbe2fbe7fc73a13549d
Kaiza Ilomo <kaiza@kunonu.co.tz>
2022-06-29 22:29:04 +0300

    Modify Vehicle model

--- vehicles/models.py
+++ vehicles/models.py
@@ -7,12 +7,16 @@ from django.utils.translation import ugettext_lazy as _
 class Vehicle(models.Model):
     """Model definition for Vehicle."""
 
+    status = (("active", "active"), ("inactive", "inactive"),)
+    category = (("bus", "bus"), ("car", "car"), ("lory", "lory"))
+
     plate_number = models.CharField(_('Plate Number'), max_length=30, help_text=_('Enter plate number of the vehicle'))
     driver = models.ForeignKey(User, on_delete=models.CASCADE)
     make = models.CharField(_("Make"), max_length=30)
     model = models.CharField(_('Vehicle Model'), max_length=30)
-    category = models.CharField(_("Category"), max_length=30)
-    status = models.CharField(_('Operation status'), max_length=30)
+    year = models.CharField(_("Vehicle Year"), max_length=4)
+    category = models.CharField(_("Category"), max_length=30, choices=category)
+    status = models.CharField(_('Operation status'), max_length=30, choices=status)
 
     class Meta:
         """Meta definition for Vehicle."""
